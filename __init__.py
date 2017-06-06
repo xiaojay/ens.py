a0c9e627fd01f952c27650b5b83b2c0b72b622f6
xiaojay <xiaojay@gmail.com>
2017-06-07 00:04:02 +0800

    fix typo

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 #coding=utf-8
-from utils imort name_hash, name_hash2
+from utils import name_hash, name_hash2
 from registrar import registrar, lookup, get_allowed_time2, get_allowed_time, start_auction, bid, unseal_bid, finalize
